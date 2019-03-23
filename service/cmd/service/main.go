81a48851b4f1a2a28c1b9fbe7a08b4f23e639b3b
Graham Cox <graham@grahamcox.co.uk>
2019-03-23 14:29:14 +0000

    Ability to add handlers to the web server

--- service/cmd/service/main.go
+++ service/cmd/service/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 	"github.com/sazzer/wyrdwest/service/internal/server"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	server := server.New()
+
+	server.Register(func(e *echo.Echo) {
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+	})
+
 	if err := server.Start(3000); err != nil {
 		logrus.WithError(err).Error("Failed to start server")
 	}
--- service/internal/server/server.go
+++ service/internal/server/server.go
@@ -7,6 +7,9 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// HandlerRegistrationFunc represents a function that can be used to register handlers with the web server
+type HandlerRegistrationFunc = func(*echo.Echo)
+
 // Server represents the actual web server to run
 type Server struct {
 	server *echo.Echo
@@ -16,12 +19,21 @@ type Server struct {
 func New() Server {
 	e := echo.New()
 
+	e.Use(middleware.RequestID())
+	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Gzip())
 	e.Use(middleware.Recover())
 
 	return Server{
 		server: e,
 	}
+}
+
+// Register allows us to register handlers with the server
+func (server *Server) Register(handlers HandlerRegistrationFunc) {
+	handlers(server.server)
 }
 
 // Start will start the server listening
