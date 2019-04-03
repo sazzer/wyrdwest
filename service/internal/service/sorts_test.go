88d650ba8a8ee96bcd454956dbee6074ce7813ad
Graham Cox <graham@grahamcox.co.uk>
2019-04-03 19:53:49 +0100

    Some minor version tweaks

--- service/internal/service/sorts_test.go
+++ service/internal/service/sorts_test.go
@@ -7,48 +7,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var sortstests = []struct {
+type test struct {
 	in  string
 	out []service.SortField
-}{
-	{"", []service.SortField{}},
-	{" ", []service.SortField{}},
-	{"name", []service.SortField{service.SortField{Field: "name", Direction: service.SortNatural}}},
-	{"name,age", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortNatural},
-		service.SortField{Field: "age", Direction: service.SortNatural},
-	}},
-	{"name, age", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortNatural},
-		service.SortField{Field: "age", Direction: service.SortNatural},
-	}},
-	{"name,     age", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortNatural},
-		service.SortField{Field: "age", Direction: service.SortNatural},
-	}},
-	{"name , age", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortNatural},
-		service.SortField{Field: "age", Direction: service.SortNatural},
-	}},
-	{" name , age ", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortNatural},
-		service.SortField{Field: "age", Direction: service.SortNatural},
-	}},
-	{"+name", []service.SortField{service.SortField{Field: "name", Direction: service.SortAscending}}},
-	{"-name", []service.SortField{service.SortField{Field: "name", Direction: service.SortDescending}}},
-	{"+name,-age", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortAscending},
-		service.SortField{Field: "age", Direction: service.SortDescending},
-	}},
-	{"+name,-age,id", []service.SortField{
-		service.SortField{Field: "name", Direction: service.SortAscending},
-		service.SortField{Field: "age", Direction: service.SortDescending},
-		service.SortField{Field: "id", Direction: service.SortNatural},
-	}},
+}
+
+func buildTests() []test {
+	return []test{
+		{"", []service.SortField{}},
+		{" ", []service.SortField{}},
+		{"name", []service.SortField{{Field: "name", Direction: service.SortNatural}}},
+		{"name,age", []service.SortField{
+			{Field: "name", Direction: service.SortNatural},
+			{Field: "age", Direction: service.SortNatural},
+		}},
+		{"name, age", []service.SortField{
+			{Field: "name", Direction: service.SortNatural},
+			{Field: "age", Direction: service.SortNatural},
+		}},
+		{"name,     age", []service.SortField{
+			{Field: "name", Direction: service.SortNatural},
+			{Field: "age", Direction: service.SortNatural},
+		}},
+		{"name , age", []service.SortField{
+			{Field: "name", Direction: service.SortNatural},
+			{Field: "age", Direction: service.SortNatural},
+		}},
+		{" name , age ", []service.SortField{
+			{Field: "name", Direction: service.SortNatural},
+			{Field: "age", Direction: service.SortNatural},
+		}},
+		{"+name", []service.SortField{{Field: "name", Direction: service.SortAscending}}},
+		{"-name", []service.SortField{{Field: "name", Direction: service.SortDescending}}},
+		{"+name,-age", []service.SortField{
+			{Field: "name", Direction: service.SortAscending},
+			{Field: "age", Direction: service.SortDescending},
+		}},
+		{"+name,-age,id", []service.SortField{
+			{Field: "name", Direction: service.SortAscending},
+			{Field: "age", Direction: service.SortDescending},
+			{Field: "id", Direction: service.SortNatural},
+		}},
+	}
 }
 
 func TestParseSorts(t *testing.T) {
-	for _, tt := range sortstests {
+	for _, tt := range buildTests() {
+		tt := tt // Pin the variable
 		t.Run(tt.in, func(t *testing.T) {
 			sorts := service.ParseSorts(tt.in)
 			assert.Equal(t, tt.out, sorts)
@@ -57,7 +62,8 @@ func TestParseSorts(t *testing.T) {
 }
 
 func BenchmarkParseSorts(b *testing.B) {
-	for _, tt := range sortstests {
+	for _, tt := range buildTests() {
+		tt := tt // Pin the variable
 		b.Run(tt.in, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				service.ParseSorts(tt.in)
