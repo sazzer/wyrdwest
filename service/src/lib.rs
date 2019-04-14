8aef391e096eac7e1a81b4bcaef77422ab70cef4
Graham Cox <graham@grahamcox.co.uk>
2019-04-14 11:29:50 +0100

    Set up workspace + bin/lib split

---
+++ service/src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate log;
+
+pub fn start() {
+    info!("Hello, world!");
+}
--- service/src/main.rs
+++ service/src/main.rs
@@ -1,9 +1,8 @@
-#[macro_use]
-extern crate log;
+extern crate wyrdwest_service;
 extern crate log4rs;
 
 fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
-    info!("Hello, world!");
+    wyrdwest_service::start()
 }
