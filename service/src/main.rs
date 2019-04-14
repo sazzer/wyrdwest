bf77ff5294eea021908279034c1b51456358105f
Graham Cox <graham@grahamcox.co.uk>
2019-04-14 10:39:51 +0100

    Set up logging

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+
 fn main() {
-    println!("Hello, world!");
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    info!("Hello, world!");
 }
