ae5523916d4a7cdbe139e91770f1243932270c32
Graham Cox <graham@grahamcox.co.uk>
2019-04-15 07:46:12 +0100

    Actually start a web server listening

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,9 +1,32 @@
 #[macro_use]
 extern crate log;
+extern crate actix_web;
 
 use std::collections::HashMap;
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+
+fn index(req: HttpRequest) -> &'static str {
+    "Hello world!"
+}
 
 pub fn start(settings: HashMap<String, String>) {
     info!("Hello, world!");
     info!("{:?}", settings);
+
+    let server = HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::cors::Cors::new())
+            .service(web::resource("/").to(index))
+    });
+
+    let port = settings.get("port")
+        .map(|port| format!("[::]:{}", port))
+        .unwrap();
+
+    server
+        .bind(port).unwrap()
+        .workers(20)
+        .run().unwrap();
 }
