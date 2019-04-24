ccc8f11e1b7d27f80a2952832cbd6d1be53f4006
Graham Cox <graham@grahamcox.co.uk>
2019-04-24 14:06:40 +0100

    Added in Fastify

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,6 +1,14 @@
-/* tslint:disable */
-function main() {
-  console.log('Hello');
+import buildServer from './server/index';
+
+function main(): void {
+  const server = buildServer();
+
+  server.listen(3000, (err, address) => {
+    if (err) {
+      throw err;
+    }
+    server.log.info(`server listening on ${address}`);
+  });
 }
 
 main();
---
+++ service/src/server/index.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+import fastifyHelmet from 'fastify-helmet';
+import fastifySensible from 'fastify-sensible';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+
+export default function buildServer(): fastify.FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> {
+  const server = fastify({
+    logger: true
+  });
+
+  server.register(fastifyCors, {
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+    origin: true,
+    preflightContinue: true
+  });
+  server.register(fastifyHelmet);
+  server.register(fastifySensible);
+
+  server.get('/', (_, reply) => {
+    reply.send({ hello: 'world' });
+  });
+
+  return server;
+}
