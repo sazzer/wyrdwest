8d14e1247b15cc1a8fcda1f271c12c77fc6a5cef
Graham Cox <graham@grahamcox.co.uk>
2019-05-02 10:27:40 +0100

    Model representation of an OAuth2 Client

---
+++ service/src/oauth2/clients/model.ts
@@ -0,0 +1,39 @@
+import { Model } from '../../service';
+import { UserID } from '../../users/model';
+
+/** Enumeration of possible response types */
+enum ResponseTypes {
+  CODE = "code",
+  TOKEN = "token",
+  ID_TOKEN = "id_token"
+};
+
+/** Enumeration of possible grant types */
+enum GrantTypes {
+  AUTHORIZATION_CODE = "authorization_code",
+  IMPLICIT = "implicit",
+  CLIENT_CREDENTIALS = "client_credentials",
+  REFRESH_TOKEN = "refresh_token"
+};
+
+/**
+ * Type representing the data in an OAuth2 Client
+ */
+export interface ClientData {
+  readonly name: string;
+  readonly secret: string;
+  readonly owner: UserID;
+  readonly redirectUris: ResponseTypes[];
+  readonly responseTypes: GrantTypes[];
+  readonly grantTypes: string[];
+}
+
+/**
+ * Type representing the ID of an OAuth2 Client
+ */
+export type ClientID = string;
+
+/**
+ * Type representing the actual client resource model
+ */
+export type ClientModel = Model<ClientID, ClientData>;
