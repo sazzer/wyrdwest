3bbc56ca4a1a0b808bb2712ffe24aa384bfb8593
Graham Cox <graham@grahamcox.co.uk>
2019-05-04 10:41:00 +0100

    Added in a bunch of middleware

--- service/src/server/index.ts
+++ service/src/server/index.ts
@@ -1,11 +1,31 @@
 import express from 'express';
 import { Express } from 'express-serve-static-core';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import connectRid from 'connect-rid';
+import cors from 'cors';
+import errorhandler from 'errorhandler';
+import morgan from 'morgan';
+import responseTime from 'response-time';
+import expressSlash from 'express-slash';
+import helmet from 'helmet';
 
 /**
  * Build the HTTP Server to work with
  */
 export default function buildServer(): Express {
   const server = express();
+
+  server.use(compression());
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(connectRid());
+  server.use(cors());
+  server.use(errorhandler());
+  server.use(morgan('combined'));
+  server.use(responseTime());
+  server.use(expressSlash());
+  server.use(helmet());
 
   return server;
 }
